0979789e56a0fe5fa1ee01e9ab9949c0905208a8
Alexey Usov <usov@mercurydevelopment.com>
2020-03-12 16:24:08 +0400

    Updates

---
+++ precache-manifest.a6f86b4a8f4a7727deac78fa73022a5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af54a82ecdd6a6abbc37b474b9f1d3d",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "89fab3ead05dcc5227b7",
+    "url": "/todo-list/static/css/main.f3f13506.chunk.css"
+  },
+  {
+    "revision": "9d843d0ec0472a656c90",
+    "url": "/todo-list/static/js/2.d4885b5a.chunk.js"
+  },
+  {
+    "revision": "37b5fb9fa73e0688add42527cf5813eb",
+    "url": "/todo-list/static/js/2.d4885b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89fab3ead05dcc5227b7",
+    "url": "/todo-list/static/js/main.bf40ab38.chunk.js"
+  },
+  {
+    "revision": "492b6893fe135a4273bc",
+    "url": "/todo-list/static/js/runtime-main.7e9ae0c4.js"
+  }
+]);
